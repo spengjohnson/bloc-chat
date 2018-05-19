9fc797eca8bcf828aa4735a6d1c88f92deb3163e
spjohnson <spgant@gmail.com>
2018-05-19 11:38:59 -0600

    troubleshooting, trying to render room list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom'; 
 import './App.css';
+import * as firebase from 'firebase'; 
+import RoomList from './components/RoomList'; 
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCjN8KjkArDS60TPELZUevcw9o5bntymWU",
+    authDomain: "bloc-chat-6d11b.firebaseapp.com",
+    databaseURL: "https://bloc-chat-6d11b.firebaseio.com",
+    projectId: "bloc-chat-6d11b",
+    storageBucket: "bloc-chat-6d11b.appspot.com",
+    messagingSenderId: "716328161162"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Welcome to Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <main>
+        <Route path="./components/RoomList" component={RoomList} /> 
+       </main>
       </div>
     );
   }
